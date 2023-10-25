e34a77c9d90caf8b0106bb3dd4ae40773c6db889
k7850 <k79357850@gmail.com>
2023-10-25 09:16:42 +0900

    메인화면 요일 선택

--- lib/ui/pages/webtoon/list_page/widgets/webtoon_list_sliver_grid.dart
+++ lib/ui/pages/webtoon/list_page/widgets/webtoon_list_sliver_grid.dart
@@ -3,20 +3,36 @@ import 'package:flutter_blog/_core/constants/size.dart';
 import 'package:flutter_blog/data/dto/webtoon_DTO/list_page_webtoon_DTO.dart';
 import 'package:flutter_blog/data/provider/param_provider.dart';
 import 'package:flutter_blog/ui/pages/webtoon/detail_page/webtoon_detail_page.dart';
+import 'package:flutter_blog/ui/pages/webtoon/list_page/webtoon_list_view_model.dart';
+import 'package:flutter_blog/ui/pages/webtoon/list_page/week_view_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../../../../common_widgets/webtoon_list_item.dart';
 
 class WebtoonListSliverGrid extends ConsumerWidget {
-  const WebtoonListSliverGrid({
+  WebtoonListSliverGrid({
     super.key,
-    required this.webtoonDTOList,
   });
-
-  final List<ListPageWebtoonDTO> webtoonDTOList;
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    WebtoonListModel? model = ref.watch(webtoonListProvider);
+    WeekModel? weekModel = ref.watch(weekProvider);
+
+    if (model == null) {
+      return Center(child: CircularProgressIndicator());
+    }
+
+    List<ListPageWebtoonDTO> webtoonDTOList = model!.webtoonDTOList!;
+
+    String week = "모두";
+    if (weekModel != null) {
+      week = weekModel.week;
+    }
+    if (week != "모두") {
+      webtoonDTOList = webtoonDTOList.where((e) => e.weekDay!.contains(week)).toList();
+    }
+
     return SliverPadding(
       padding: EdgeInsets.symmetric(horizontal: sizePaddingLR17),
       sliver: SliverGrid(
@@ -32,8 +48,7 @@ class WebtoonListSliverGrid extends ConsumerWidget {
                 onTap: () {
                   ParamStore paramStore = ref.read(paramProvider);
                   paramStore.addWebtoonDetailId(webtoonDTOList[index].id);
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (_) => WebtoonDetailPage()));
+                  Navigator.push(context, MaterialPageRoute(builder: (_) => WebtoonDetailPage()));
                   // print("실행됨");
                 },
                 child: WebtoonListItem(webtoonDTO: webtoonDTOList[index]));
