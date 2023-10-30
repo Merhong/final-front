bfee0d8e18fb73edda698696bd6e643356a82063
Merhong <zx2938@gmail.com>
2023-10-30 11:29:13 +0900

    [김대홍] My페이지 바텀네비게이션바 수정

--- lib/ui/pages/other/my_page/my_page.dart
+++ lib/ui/pages/other/my_page/my_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/ui/common_widgets/app_bottom.dart';
 import 'package:flutter_blog/ui/common_widgets/myapp_appbar.dart';
 import 'package:flutter_blog/ui/pages/other/my_page/wigets/my_body_tab_bar.dart';
 
@@ -9,15 +10,13 @@ void main() {
 class MyPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        appBar: MyappAppbar(),
-        body: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 17), // 양쪽 여백을 17로 설정
-          child: MyBodyTabBar(),
-        ),
+    return Scaffold(
+      appBar: MyappAppbar(),
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 17), // 양쪽 여백을 17로 설정
+        child: MyBodyTabBar(),
       ),
+      bottomNavigationBar: AppBottom(),
     );
   }
 }
