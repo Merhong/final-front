178dc0e368c6333547d38fa9463237098481b2d7
YHYELIM <yhl0513@naver.com>
2023-10-30 17:08:46 +0900

    cardmethod앱바 수정

--- lib/ui/pages/pay/pay_method_page/pay_method_page.dart
+++ lib/ui/pages/pay/pay_method_page/pay_method_page.dart
@@ -7,57 +7,53 @@ class PayMethodPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(10),
-      child: Scaffold(
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 0,
         backgroundColor: Colors.white,
-        appBar: AppBar(
-          elevation: 0,
-          backgroundColor: Colors.white,
-          title: Text(
-            "Hyundai Card",
-            style: TextStyle(color: Colors.black),
+        title: Text(
+          "Hyundai Card",
+          style: TextStyle(color: Colors.black),
+        ),
+        actions: [
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              "간편결제",
+              style:
+                  TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+            ),
           ),
-          actions: [
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                "간편결제",
-                style:
-                    TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
-              ),
+          Container(
+            width: 1,
+            height: 5,
+            color: Colors.grey,
+          ),
+          TextButton(
+            onPressed: () {},
+            child: Text(
+              "페이결제",
+              style: TextStyle(fontWeight: FontWeight.bold, color: Colors.grey),
             ),
-            Container(
-              width: 1,
-              height: 5,
-              color: Colors.grey,
-            ),
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                "페이결제",
-                style:
-                    TextStyle(fontWeight: FontWeight.bold, color: Colors.grey),
-              ),
-            ),
-          ],
-        ),
-        body: Column(
-          children: [
-            SizedBox(height: 8),
-            PayMethodBlackButton(),
-            SizedBox(height: 5),
-            PayMethodWhiteButton(
-              payment: "PIN번호",
-              smallFont: "6자리 숫자로 간편하게 (PayShot 포함)",
-            ),
-            SizedBox(height: 5),
-            PayMethodWhiteButton(
-              payment: "일반",
-              smallFont: "카드번호로 결제 (기존 등록 카드만 가능)",
-            ),
-          ],
-        ),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          SizedBox(height: 8),
+          PayMethodBlackButton(),
+          SizedBox(height: 5),
+          PayMethodWhiteButton(
+            payment: "PIN번호",
+            smallFont: "6자리 숫자로 간편하게 (PayShot 포함)",
+          ),
+          SizedBox(height: 5),
+          PayMethodWhiteButton(
+            payment: "일반",
+            smallFont: "카드번호로 결제 (기존 등록 카드만 가능)",
+          ),
+        ],
       ),
     );
   }
