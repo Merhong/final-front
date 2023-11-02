adb3fa1c875d47a258ef07bf3095edf7e532a0b8
heidi505 <xxwhite19@gmail.com>
2023-11-02 12:41:10 +0900

    베ã도 통신 조ê다른걸로 해봄 , 베도 그림 ì수정

--- lib/_core/constants/size.dart
+++ lib/_core/constants/size.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 const double sizeS5 = 5.0;
 const double sizeM10 = 10.0;
+const double sizeM17 = 17.0;
 const double sizeL20 = 20.0;
 const double sizeML20 = 30.0;
 const double sizeXL50 = 50.0;
