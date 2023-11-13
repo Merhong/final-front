6b71e306b825ba1bdefb8096630266f56f81b2b4
heidi505 <xxwhite19@gmail.com>
2023-11-13 16:08:45 +0900

    ..

--- lib/ui/pages/webtoon/list_page/widgets/webtoon_list_body.dart
+++ lib/ui/pages/webtoon/list_page/widgets/webtoon_list_body.dart
@@ -3,10 +3,8 @@ import 'package:flutter_blog/data/dto/webtoon_DTO/list_page_webtoon_DTO.dart';
 import 'package:flutter_blog/ui/common_widgets/bottom_rule.dart';
 import 'package:flutter_blog/ui/pages/webtoon/list_page/webtoon_list_view_model.dart';
 import 'package:flutter_blog/ui/pages/webtoon/list_page/widgets/webtoon_list_bottom_random.dart';
-import 'package:flutter_blog/ui/pages/webtoon/list_page/widgets/webtoon_list_bottom_view.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'webtoon_list_middle_advertising.dart';
 import 'webtoon_list_sliver_appbar_and_pageview.dart';
 import 'webtoon_list_sliver_grid.dart';
 import 'webtoon_list_sliver_persistent_header.dart';
@@ -32,7 +30,7 @@ class WebtoonListBody extends ConsumerWidget {
       slivers: <Widget>[
         WebtoonListSliverAppBarAndPageView(isScroll),
         WebtoonListSliverPersistentHeader(),
-        WebtoonListMiddleAdvertising(),
+        // WebtoonListMiddleAdvertising(),
         WebtoonListSliverGrid(),
         // SliverToBoxAdapter(
         //   child: Container(
