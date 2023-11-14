6b1e6e19976cd95a3c328074f37a97d9a7c57bb0
Merhong <zx2938@gmail.com>
2023-11-14 11:51:45 +0900

    베스트도전 웹툰 상세보기 연결 완료

--- lib/ui/common_widgets/custom_best_view.dart
+++ lib/ui/common_widgets/custom_best_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_blog/_core/constants/http.dart';
+import 'package:flutter_blog/_core/constants/move.dart';
 import 'package:flutter_blog/data/dto/webtoon_dto/best_DTO.dart';
+import 'package:flutter_blog/data/provider/param_provider.dart';
 import 'package:flutter_blog/ui/pages/webtoon/best_page/webtoon_best_view_model.dart';
+import 'package:flutter_blog/ui/pages/webtoon/detail_page/webtoon_detail_page.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/intl.dart';
 
@@ -22,75 +25,83 @@ class CustomBestView extends ConsumerWidget {
     return ListView.builder(
       itemCount: bestList.length,
       itemBuilder: (context, index) {
-        return Padding(
-          padding: const EdgeInsets.only(top: 2),
-          child: Container(
-            height: 120,
-            color: Colors.white,
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ClipRRect(
-                  borderRadius: BorderRadius.circular(5),
-                  child: Image.network(
-                    "${imageURL}/WebtoonThumbnail/${bestList[index].image}",
-                    fit: BoxFit.cover,
-                    errorBuilder: (context, error, stackTrace) {
-                      return Image.asset(
-                        "assets/default_webtoon_Thumbnail.jpg",
-                        fit: BoxFit.cover,
-                      );
-                    },
+        return InkWell(
+          onTap: () {
+            ParamStore paramStore = ref.read(paramProvider);
+            paramStore.addWebtoonDetailId(bestList[index].id);
+            Navigator.push(
+                context, MaterialPageRoute(builder: (_) => WebtoonDetailPage()));
+          },
+          child: Padding(
+            padding: const EdgeInsets.only(top: 2),
+            child: Container(
+              height: 120,
+              color: Colors.white,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(5),
+                    child: Image.network(
+                      "${imageURL}/WebtoonThumbnail/${bestList[index].image}",
+                      fit: BoxFit.cover,
+                      errorBuilder: (context, error, stackTrace) {
+                        return Image.asset(
+                          "assets/default_webtoon_Thumbnail.jpg",
+                          fit: BoxFit.cover,
+                        );
+                      },
+                    ),
                   ),
-                ),
-                SizedBox(width: 10),
-                // 중앙에 제목과 날짜 배치
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "${bestList[index].title}",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      "${bestList[index].authorNicknameList}",
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                    Text(
-                      "시트콤 같은 일상툰",
-                      style: TextStyle(
-                        color: Colors.black,
-                      ),
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.star,
-                          color: Colors.red,
+                  SizedBox(width: 10),
+                  // 중앙에 제목과 날짜 배치
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "${bestList[index].title}",
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
                         ),
-                        Text(
-                          "${(bestList[index].starScore! / (bestList[index].starCount == 0 ? 1 : bestList[index].starCount!)).toStringAsFixed(2)}",
-                          style: TextStyle(
+                      ),
+                      Text(
+                        "${bestList[index].authorNicknameList}",
+                        style: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                      // Text(
+                      //   "시트콤 같은 일상툰",
+                      //   style: TextStyle(
+                      //     color: Colors.black,
+                      //   ),
+                      // ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.star,
                             color: Colors.red,
                           ),
-                        ),
-                        Text(
-                          "   ${DateFormat('yyyy-MM-dd').format(bestList[index].episodeUpdatedAt!)}",
-                          style: TextStyle(
-                            color: Colors.black,
+                          Text(
+                            "${(bestList[index].starScore! / (bestList[index].starCount == 0 ? 1 : bestList[index].starCount!)).toStringAsFixed(2)}",
+                            style: TextStyle(
+                              color: Colors.red,
+                            ),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-              ],
+                          Text(
+                            "   ${DateFormat('yyyy-MM-dd').format(bestList[index].episodeUpdatedAt!)}",
+                            style: TextStyle(
+                              color: Colors.black,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ),
         );
